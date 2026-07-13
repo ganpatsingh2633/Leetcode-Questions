65e87fb551c9e948dd5f2ca763e6f4b28d464938
ganpatsingh2633 <156999260+ganpatsingh2633@users.noreply.github.com>
2026-07-13 16:11:55 +0530

    Time: 1160 ms (5.03%), Space: 19.4 MB (21.54%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,13 +1,16 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def distinct(a,b,c,d):
+            return a != b and a!=c and a!=d and b!=c and b!=d and c!=d
+        h = {}
+        for i in range(len(nums)):
+            h[nums[i]] = i
         ans = set()
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                seen =set()
                 for k in range(j+1,len(nums)):
                     req = target - (nums[i] + nums[j] + nums[k])
-                    if req in seen :
+                    if req in h and distinct(i,j,k, h[req]) :
                         x = tuple(sorted([nums[i], nums[j], nums[k], req]))
                         ans.add(x)
-                    seen.add(nums[k])
         return [ i for i in ans]
