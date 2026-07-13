8a7a6d724959017978b5bc9502b6558c97dcaff8
ganpatsingh2633 <156999260+ganpatsingh2633@users.noreply.github.com>
2026-07-13 16:21:21 +0530

    Time: 0 ms (100%), Space: 19.5 MB (38.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,3 +1,30 @@
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        left = 0
+        right = rows * cols - 1
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            row = mid // cols
+            col = mid % cols
+
+            if matrix[row][col] == target:
+                return True
+
+            elif matrix[row][col] < target:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return False
 # class Solution:
 #     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         # row = -1
@@ -8,19 +35,3 @@
         #     if matrix[row][i] == target:
         #         return True
         # return False
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        i , j = 0 , (rows * cols) -1
-        while i <= j :
-            mid = (i + j) // 2
-            row  = mid // cols
-            col = mid % cols
-            if matrix[row][col] == target:
-                return True
-            elif matrix[row][col] < target:
-                i = mid + 1
-            else : 
-                j = mid - 1
-        return False
