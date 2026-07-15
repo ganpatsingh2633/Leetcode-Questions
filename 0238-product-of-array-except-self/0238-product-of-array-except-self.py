6288a587951c0c76ea3ff9b40db300658f9bfb64
ganpatsingh2633 <156999260+ganpatsingh2633@users.noreply.github.com>
2026-07-15 16:57:34 +0530

    Time: 11 ms (99.32%), Space: 25.3 MB (81.24%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,34 @@
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         r = [0] * len(nums)
+#         l = [0] * len(nums)
+#         r[-1] = 1
+#         l[0] = 1
+#         ans = []
+#         for i in range(len(nums)-2,-1,-1):
+#             r[i] = r[i+1] * nums[i+1]
+#         for i in range(1 ,len(nums)):
+#             l[i] = l[i-1] * nums[i-1]
+#         for i in range(len(nums)):
+#             ans.append(r[i] * l[i])
+#         return ans
+
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        r = [0] * len(nums)
-        l = [0] * len(nums)
-        r[-1] = 1
-        l[0] = 1
-        ans = []
-        for i in range(len(nums)-2,-1,-1):
-            r[i] = r[i+1] * nums[i+1]
-        for i in range(1 ,len(nums)):
-            l[i] = l[i-1] * nums[i-1]
-        for i in range(len(nums)):
-            ans.append(r[i] * l[i])
-        return ans
+        product_all = 1
+        n_zero = 0
+        for num in nums:
+            product_all *= num
+            if num == 0:
+                n_zero += 1
+        if n_zero == 0:
+            return [product_all//num for num in nums]
+        if n_zero == 1:
+            product_without_zero = 1
+            for num in nums:
+                if num != 0:
+                    product_without_zero *= num
+            return [0 if num != 0 else product_without_zero for num in nums]
+        if n_zero > 1:
+            return [0 for num in nums]
